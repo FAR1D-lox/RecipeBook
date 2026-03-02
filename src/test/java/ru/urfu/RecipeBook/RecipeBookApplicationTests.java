84e70e72188cdf8a71721cb819d17d32dee8da4f
FAR1D-lox <tarasburacenko@gmail.com>
2026-03-02 17:50:19 +0500

    initialize Spring Boot project with basic structure - Create project 'Recipe Book' - Add .gitignore, README.md

---
+++ src/test/java/ru/urfu/RecipeBook/RecipeBookApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.urfu.RecipeBook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
