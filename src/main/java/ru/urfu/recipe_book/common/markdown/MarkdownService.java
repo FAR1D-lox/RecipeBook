6a99ac8b120efbc669280e5cc97c91dac430ec72
dislert <konstantintropin93@gmail.com>
2026-04-27 16:11:46 +0500

    added markdown to html render

--- src/main/java/ru/urfu/recipe_book/common/markdown/MarkdownService.java
+++ src/main/java/ru/urfu/recipe_book/common/markdown/MarkdownService.java
@@ -1,10 +1,25 @@
 package ru.urfu.recipe_book.common.markdown;
 
-
+import com.vladsch.flexmark.html.HtmlRenderer;
+import com.vladsch.flexmark.parser.Parser;
+import com.vladsch.flexmark.util.data.MutableDataSet;
+import com.vladsch.flexmark.util.ast.Node;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-@Service
 @RequiredArgsConstructor
+@Service
 public class MarkdownService {
+    public String render(String markdown) {
+        if (markdown == null || markdown.isBlank()) {
+            return "";
+        }
+
+        MutableDataSet options = new MutableDataSet();
+        Parser parser = Parser.builder(options).build();
+        HtmlRenderer renderer = HtmlRenderer.builder(options).build();
+
+        Node document = parser.parse(markdown);
+        return renderer.render(document);
+    }
 }
