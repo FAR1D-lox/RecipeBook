5ff3de63c10844bccd10e41634a0032ef9d19015
dislert <konstantintropin93@gmail.com>
2026-06-02 17:30:45 +0500

    added validation for recipe and comment dto

--- src/main/java/ru/urfu/recipe_book/recipe/dto/UpdateRecipeDto.java
+++ src/main/java/ru/urfu/recipe_book/recipe/dto/UpdateRecipeDto.java
@@ -1,5 +1,6 @@
 package ru.urfu.recipe_book.recipe.dto;
 
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -16,18 +17,29 @@ import java.util.Set;
 @NoArgsConstructor
 @AllArgsConstructor
 public class UpdateRecipeDto {
+    @NotBlank
+    @Size(min = 3, max = 200, message = "Название должно быть от 3 до 200 символов")
     private String title;
 
+    @Size(max = 5000, message = "Описание не может превышать 5000 символов")
     private String description;
 
+    @Size(max = 500, message = "URL изображения не может превышать 500 символов")
     private String imageUrl;
 
+    @NotNull(message = "Уровень сложности должен быть указан")
     private DifficultyLevel difficultyLevel;
 
+    @NotNull(message = "Время подготовки должно быть указано")
+    @Positive(message = "Время подготовки должно быть положительным числом")
     private Integer preparationTime;
 
+    @NotNull(message = "Время готовки должно быть указано")
+    @Positive(message = "Время готовки должно быть положительным числом")
     private Integer cookingTime;
 
     private Set<PreferenceTag> tags = new HashSet<>();
+
+    @NotNull(message = "Время приёма пищи должно быть указано")
     private MealTime mealTime;
 }
